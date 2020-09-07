4e405e1cc5d8a2d9e0e2f1c192b76660def733e8
Chirag Vasani <55270861+chiragvasani36@users.noreply.github.com>
2020-09-07 14:03:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region  = "us-east-1"
   access_key= "__aws_access_key_id__"
   secret_key= "__aws_secret_access_key__"
-  profile = "default"
-}
-provider "aws" {
-    alias="mumbai"
-  region  = "ap-south-1"
-  profile = "default"
+  
 }
 
