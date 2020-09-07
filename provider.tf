143d543bad3f23aa2fe332e5fffb27bdd9fd9c68
Chirag Vasani <55270861+chiragvasani36@users.noreply.github.com>
2020-09-07 17:23:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region  = "us-east-1"
-  access_key= "__aws_access_key_id__"
-  secret_key= "__aws_secret_access_key__"
+  access_key= "__aws_access_key_id1__"
+  secret_key= "__aws_secret_access_key1__"
   
 }
 
